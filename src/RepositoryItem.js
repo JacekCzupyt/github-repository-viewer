fca91c4a5f5bf847cea6efb8b4e87690e75a4740
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-25 18:47:31 +0200

    Added link to repository

--- src/RepositoryItem.js
+++ src/RepositoryItem.js
@@ -1,7 +1,11 @@
 function RepositoryList(props){
     return(
         <tr>
-            <td>{props.repo.name}</td>
+            <td>
+                <a href={props.repo.html_url} style={{color: "white"}}>
+                    {props.repo.name}
+                </a>
+            </td>
             <td>{props.repo.owner.login}</td>
             <td>{props.repo.description}</td>
             <td>{props.repo.stargazers_count}</td>
