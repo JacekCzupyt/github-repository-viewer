0ea6a1833b5abb1119dc6d4ba447f75c0c8d0f61
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-25 16:38:43 +0200

    Added error handling

--- src/RepositoryList.js
+++ src/RepositoryList.js
@@ -6,15 +6,25 @@ function RepositoryList(props){
     const [repos, setRepos] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
     const [username, setUsername] = useState("JacekCzupyt");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const LoadData = () => {
-        console.log(username);
         setIsLoaded(false);
         fetch("https://api.github.com/users/" + username + "/repos")
-            .then(res => res.json())
+            .then(res => {
+                if(res.ok){
+                    return res.json();
+                }
+                throw new Error("Request failed with code " + res.status)
+            })
             .then(data => setRepos(data))
             .then(() => setIsLoaded(true))
-            .catch(error => console.error(error));
+            .then(() => setErrorMessage(""))
+            .catch(err => {
+                console.error(err);
+                setErrorMessage("User not found");
+                setIsLoaded(true);
+            });
     }
 
     useEffect(() => {
@@ -25,9 +35,17 @@ function RepositoryList(props){
         <div>
             {isLoaded ?
             <div>
-                <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
+                <input type="text" name="GithubUser" placeholder="GitHub Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
                 <button onClick={LoadData}>Search</button>
-                {repos.sort((r1, r2) => r2.stargazers_count - r1.stargazers_count).map((repo) => <div><p>{repo.name}</p></div>)}
+                {errorMessage === "" ? 
+                (
+                    repos.length > 0 ?
+                    repos.sort((r1, r2) => r2.stargazers_count - r1.stargazers_count).map((repo) => <div><p>{repo.name}</p></div>)
+                    :
+                    <p>User has not repositories</p>
+                )
+                :
+                <p>{errorMessage}</p>}
             </div>
             :
             "Loading..."
